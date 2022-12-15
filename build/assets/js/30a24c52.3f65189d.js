205a4dddf4ae9919e7b95e5b0113450e49c18795
Blake Bishop <blakesterbishop@gmail.com>
2022-12-15 12:49:46 -0700

    Remove build from .gitignore for pages

---
+++ build/assets/js/30a24c52.3f65189d.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkprenda_engineering=self.webpackChunkprenda_engineering||[]).push([[453],{8605:e=>{e.exports=JSON.parse('{"label":"hello","permalink":"/blog/tags/hello","allTagsPath":"/blog/tags","count":2}')}}]);
