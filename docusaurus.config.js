66aafc922088f6a1996499bbe688c95e4d83412c
Blake Bishop <blakesterbishop@gmail.com>
2022-12-16 10:25:12 -0700

    Update path for edit link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -36,10 +36,8 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/prenda-school/prenda-school.github.io/tree/main/docs',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
